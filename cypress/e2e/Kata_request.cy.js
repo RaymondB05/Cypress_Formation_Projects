df415b07796d47508b37c809fe4ac475c324e4e8
RaymondB05 <101431825+RaymondB05@users.noreply.github.com>
2024-01-23 14:36:47 +0100

    Utilisation des data attributs

--- cypress/e2e/Kata_request.cy.js
+++ cypress/e2e/Kata_request.cy.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
+
 const { faker } = require('@faker-js/faker');
 const cheminFichier = 'C:/Users/user/Desktop/AleeConseil_projects/Formulaire_Kata/demo-form-html-css/index.html';
-
 
 const fname = faker.name.firstName();
 const lname = faker.name.lastName();
@@ -33,7 +33,6 @@ describe("Scénario de test 1 " , () => {
 
     });  
 
-
 describe("Scénario de test  2" , () => {
 
     it( " Cliquer sur 'Envoyer' / ne pas remplir tous les champs obligatoires / Un message d'information apparaît  " , () => {
@@ -41,54 +40,53 @@ describe("Scénario de test  2" , () => {
         // Remplissage de chaque champs du formulaire avec une valeur aléatoire générée par le Faker !
         cy.visit('index.html');
 
-        cy.get("#lastName").type(lname);
-        cy.get("#lastName").should("be.visible").and("have.value", lname);
+        cy.get('[data-cy="lastName"]').type(lname);
+        cy.get('[data-cy="lastName"]').should("be.visible").and("have.value", lname);
 
 
 
-        cy.get("#firstName").type(fname);
-        cy.get("#firstName").should("be.visible").and("have.value", fname);
+        cy.get('[data-cy="firstName"]').type(fname);
+        cy.get('[data-cy="firstName"]').should("be.visible").and("have.value", fname);
 
 
 
-        cy.get("#email").type(email);
-        cy.get("#email").should("be.visible").and("have.value", email);
+        cy.get('[data-cy="email"]').type(email);
+        cy.get('[data-cy="email"]').should("be.visible").and("have.value", email);
 
 
 
-        // cy.get("#age").type(age);
-        // cy.get("#age").should("be.visible").and("have.value", age);
+        // cy.get('[data-cy="age"]').type(age);
+        // cy.get('[data-cy="age"]').should("be.visible").and("have.value", age);
 
 
 
         // Cocher l'element Radio qui contient la valeur Yes + test du check
-           cy.get("[value=yes]").check();
-           cy.get('.radio input[value="yes"]').should('be.checked');
+           cy.get('[data-cy="radio-yes"]').check();
+           cy.get('[data-cy="radio-yes"]').should('be.checked');
 
 
         // Cocher le checkbox qui contient la valeur Cypress + test du check 
 
-          cy.get('.checkbox [value="cypress"]').check();
-          cy.get('.checkbox [value="cypress"]').should('be.checked');
+          cy.get('[data-cy="checkboxCypress"]').check();
+          cy.get('[data-cy="checkboxCypress"]').should('be.checked');
 
 
         // Sélectionner l'option 'Dur' dans le menu déroulant
-        cy.get('select[name="level"]').select('high');
-        cy.get('select[name="level"]').should('have.value', 'high');
+        cy.get('[data-cy="level"]').select('high');
+        cy.get('[data-cy="level"]').should('have.value', 'high');
 
          // Insérer la phrase de suggestion 
-         cy.get('textarea[name="suggestion"]').type(phraseAleatoire);
-         cy.get('textarea[name="suggestion"]').should('have.value', phraseAleatoire);
+         cy.get('[data-cy="suggestion"]').type(phraseAleatoire);
+         cy.get('[data-cy="suggestion"]').should('have.value', phraseAleatoire);
 
 
           // Cliquer sur le bouton 'Envoyer'
-          cy.contains('button', 'Envoyer').click();
-          cy.get('#notification').should('have.class', 'show');
+          cy.get('[data-cy="envoyer"]').click();
+          cy.get('[data-cy="notification"]').should('have.class', 'show');
 
     }); 
     
 });
-
 
 describe("Scénario de test  3" , () => {
 
@@ -97,54 +95,53 @@ describe("Scénario de test  3" , () => {
         // Remplissage de chaque champs du formulaire avec une valeur aléatoire générée par le Faker !
         cy.visit('index.html');
 
-        cy.get("#lastName").type(lname);
-        cy.get("#lastName").should("be.visible").and("have.value", lname);
+        cy.get('[data-cy="lastName"]').type(lname);
+        cy.get('[data-cy="lastName"]').should("be.visible").and("have.value", lname);
 
 
 
-        cy.get("#firstName").type(fname);
-        cy.get("#firstName").should("be.visible").and("have.value", fname);
+        cy.get('[data-cy="firstName"]').type(fname);
+        cy.get('[data-cy="firstName"]').should("be.visible").and("have.value", fname);
 
 
 
-        cy.get("#email").type(email);
-        cy.get("#email").should("be.visible").and("have.value", email);
+        cy.get('[data-cy="email"]').type(email);
+        cy.get('[data-cy="email"]').should("be.visible").and("have.value", email);
 
 
 
-        cy.get("#age").type(age);
-        cy.get("#age").should("be.visible").and("have.value", age);
+        cy.get('[data-cy="age"]').type(age);
+        cy.get('[data-cy="age"]').should("be.visible").and("have.value", age);
 
 
 
         // Cocher l'element Radio qui contient la valeur Yes + test du check
-           cy.get("[value=yes]").check();
-           cy.get('.radio input[value="yes"]').should('be.checked');
+           cy.get('[data-cy="radio-yes"]').check();
+           cy.get('[data-cy="radio-yes"]').should('be.checked');
 
 
         // Cocher le checkbox qui contient la valeur Cypress + test du check 
 
-          cy.get('.checkbox [value="cypress"]').check();
-          cy.get('.checkbox [value="cypress"]').should('be.checked');
+          cy.get('[data-cy="checkboxCypress"]').check();
+          cy.get('[data-cy="checkboxCypress"]').should('be.checked');
 
 
         // Sélectionner l'option 'Dur' dans le menu déroulant
-        cy.get('select[name="level"]').select('high');
-        cy.get('select[name="level"]').should('have.value', 'high');
+        cy.get('[data-cy="level"]').select('high');
+        cy.get('[data-cy="level"]').should('have.value', 'high');
 
          // Insérer la phrase de suggestion 
-         cy.get('textarea[name="suggestion"]').type(phraseAleatoire);
-         cy.get('textarea[name="suggestion"]').should('have.value', phraseAleatoire);
+         cy.get('[data-cy="suggestion"]').type(phraseAleatoire);
+         cy.get('[data-cy="suggestion"]').should('have.value', phraseAleatoire);
 
 
           // Cliquer sur le bouton 'Envoyer'
-          cy.contains('button', 'Envoyer').click();
-          cy.get('#notification').should('not.have.class', 'show');
+          cy.get('[data-cy="envoyer"]').click();
+          cy.get('[data-cy="notification"]').should('not.have.class', 'show');
 
     }); 
     
 });
-
 
 
 Cypress.on('uncaught:exception', (err, runnable) => {
@@ -153,5 +150,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // Optionally, prevent Cypress from failing the test
     return false;
   });
+
+
   
 
